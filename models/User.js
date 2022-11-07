d208d063aab55996c68097f40fa676cfe68f1fbe
Alex Oleynik <alexoleynik74@gmail.com>
2022-11-07 17:00:37 -0600

    initial commit, files created

---
+++ models/User.js
@@ -0,0 +1,38 @@
+const {Schema, model} = require('mongoose');
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trimmed: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
+    },
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+    }],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }],
+},
+{
+    toJson: {
+        getters: true,
+        virtuals: true
+    },
+},
+);
+
+userSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
+});
+
+const User = model('user', userSchema);
+module.exports = User;
